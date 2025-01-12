79988affd89e26e39c7b608936efee60ebb4b981
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-01-12 17:51:51 +0530

    adding project info

--- src/components/OfficialProjects.jsx
+++ src/components/OfficialProjects.jsx
@@ -5,18 +5,21 @@ import arrowBlack from '../assets/arrowBlack.png'
 
 const OfficialProjects = () => {
 
-  const [expandedCard1, setExpanded1] = useState(false);
-  const [expandedCard2, setExpanded2] = useState(false);
+  const [showOptiusInfo, setShowOptiusInfo] = useState(false);
+  const [showDevopsInfo, setShowDevopsInfo] = useState(false);
+  const [showCopilotInfo, setShowCopilotInfo] = useState(false);
 
-  const handleToggle1 = () => {
-    setExpanded1(!expandedCard1);
-    // setExpanded2(!expandedCard2);
+  const handleShowOptiusInfo = () => {
+    setShowOptiusInfo(!showOptiusInfo);
   };
 
-  const handleToggle2 = () => {
-    setExpanded2(!expandedCard2);
-    // setExpanded1(!expandedCard1)
+  const handleShowDevopsInfo = () => {
+    setShowDevopsInfo(!showDevopsInfo);
   };
+
+  const handleShowCopilotInfo = () => {
+    setShowCopilotInfo(!showCopilotInfo);
+  }
 
 
 
@@ -24,14 +27,30 @@ const OfficialProjects = () => {
     <div className='official-projects-background'>
     <div className='off-title-text'>Official projects:</div>
     <div class="card-container">
-  <div class="card">Optius Order removal tool UI<button className='button-1'><img  className='arrow-1'/></button></div>
-  <div class="card">Devops Automation UI<button className='button-2'><img className='arrow-2'/></button></div>
-  <div class="card">Copilot UI<button className='button-3'><img className='arrow-3'/></button></div>
+  <div class="card">Optius Order removal tool<button className='button-1' onClick={handleShowOptiusInfo}><img  className='arrow-1'/></button></div>
+  <div class="card">Devops Automation<button className='button-2' onClick={handleShowDevopsInfo}><img className='arrow-2'/></button></div>
+  <div class="card">Copilot<button className='button-3'onClick={handleShowCopilotInfo}><img className='arrow-3'/></button></div>
 </div>
 <div className='slanted-div-projects'>
  
 </div>
-     
+     {showOptiusInfo && (
+      <>
+      <div className='optius-project-title'> Project title : Optius Order Removal Tool</div>
+      <div className='optius-project-description'>A web app made for Lumen to optimize order disconnection and cancellation processes, reducing paperwork by 70%. </div>
+      <div className='optius-project-technologies'>
+       
+        <div className='technologies-used'>Technologies used : </div>
+        <ol className='technologies-list'>
+        <li>Front end : React.js</li>
+        <li>Back end : Spring boot</li>
+        <li>Database : MySQL</li>
+        <li>Others : GCP.</li>
+        </ol>
+      </div>
+      </>
+     )
+     }
     </div>
   )
 }
